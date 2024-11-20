d28384b8f095135b022e0eebed0726ec0c3ebd47
Demiyt <dengames2306@gmail.com>
2024-11-20 15:55:38 +0200

    Added comments

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import os
 import glob
 import time
 
-# Підключення до сенсора
+# import libraries
+
+
+
+# Connection to the temperature sensor
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
@@ -11,10 +15,12 @@ base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
 
+# Opening a data file
 def read_temp_raw():
     with open(device_file, 'r') as f:
         return f.readlines()
 
+# Temperature reading
 def read_temp():
     lines = read_temp_raw()
     while lines[0].strip()[-3:] != 'YES':
@@ -28,57 +34,52 @@ def read_temp():
 
 
 
-heater_gpio = gpio0.LED(23)
-heating = False
+heater_gpio = gpio0.LED(23)    # Heater relay control pin
+heating = False  # Heater status flag
 
 
+# Prompt user to set the temperature range
+print("Enter the temperature range to maintain:")
+min_temp = float(input("Minimum value: "))
+max_temp = float(input("Maximum value: "))
 
-print("Напишіть діапазон, в якому треба підтримувати температуру")
-min_t = float(input("Мінімальне значення: "))
-max_t = float(input("Максимальне значення: "))
 
+heater_off_temp = max_temp
 
-heater_off_temp = 0
-temps = [max_t]
+temps = [max_temp]      # list of temperature values
 write_temp_to_list = False
-
-
 
 
 print("\n\n")
 
 
-
 while True:
     try:
+        # getting temperature value
         temp_c = read_temp()
         print(f'Температура: {temp_c} °C')
-        time.sleep(1)
-
 
         if write_temp_to_list:
             temps.append(temp_c)
 
-
-
-
-        if temp_c <= min_t and not heating:
+        if temp_c <= min_temp and not heating:
             write_temp_to_list = True
-            heater_off_temp = max_t - ((max(temps) - max_t) * (max_t / max(temps)))
+            heater_off_temp = max_temp - ((max(temps) - max_temp) * (max_temp / max(temps)))
 
-
-            print(max(temps), max_t)
-            print(heater_off_temp)
-
-
-
+            # turning on the heater
             print("\nHEATER ON\n")
             heater_gpio.on()
             heating = True
+
         if temp_c >= heater_off_temp and heating:
+            # turning off the heater
             print("\nHEATER OFF\n")
-            heater_gpio.off()  # Закрити реле
+            heater_gpio.off()
             heating = False
+
+        time.sleep(1)
+
+
     except Exception as e:
-        print("Помилка читання температури:", e)
+        print("Temperature reading error:", e)
         time.sleep(1)
